01ec3a4c346985f527b6fcde72f5737a7f356dbc
Tom Ridge <tom.j.ridge@googlemail.com>
2015-12-18 14:05:46 +0000

    +

---
+++ src/Tom_proof.thy
@@ -0,0 +1,19 @@
+theory Tom_proof
+imports Insert_tree_stack
+begin
+
+definition invariant_wf_ts :: "bool" where
+"invariant_wf_ts == (
+! ts.
+wellformed_ts ts \<longrightarrow> 
+(
+let ts' = step_tree_stack ts in
+case ts' of 
+None \<Rightarrow> True
+| Some ts' \<Rightarrow> (
+wellformed_ts ts'
+)
+))
+"
+
+end
