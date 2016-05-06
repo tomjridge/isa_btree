c3096f08c8f3767d440e81f8263d88ff07d0eb56
ag91 <agiugliano@live.it>
2016-05-06 12:05:52 +0100

    Add first delete definitions
    We are keeping all definitions in this file because it is a prototype.
    The aim is to divide a delete ascension in steps were focus and context
    are both well formed.
    The idea is that a single step does not affect the Tree (focus = DUp) or
    changes it (focus = DDelete): in both cases the given focus should
    contain well formed trees.

---
+++ src/Delete_tree_stack.thy
@@ -0,0 +1,92 @@
+theory Delete_tree_stack
+imports Tree Insert_tree_stack
+begin
+(*I need new datatypes in tree stack*)
+
+
+
+(*BEGIN -this should go in the Utils theory*)
+(*remove nth from list*)
+fun remove_at :: "nat => 'a list => 'a list" where
+"remove_at i [] = []" |
+"remove_at 0 (x#xs) = xs" |
+"remove_at (Suc i) (x#xs) = x#(remove_at i xs)"
+
+definition remove_key_child :: "nat => Tree => Tree" where
+"remove_key_child i t == (
+case t of 
+Leaf ls => Leaf (remove_at i ls)
+| Node (ks,rs) => (*when I remove from a Node, there has been a merge, so I know I MUST delete a key and a child*)
+  if i = length rs 
+  then Node((remove_at (i-1) ks),(remove_at i rs))
+  else Node((remove_at i ks),(remove_at i rs))
+)
+"
+(*END - this should go in the Utils theory*)
+
+(*BEGIN - this should go in a del_tree_stack theory*)
+
+(*begin delete types*)
+datatype up_or_delete = DUp "Tree" | DDelete "(Tree * nat)"
+
+type_synonym del_focus_t = "up_or_delete"
+
+datatype del_tree_stack = Del_tree_stack "del_focus_t * context_t"
+
+definition dest_del_ts :: "del_tree_stack => (del_focus_t * context_t)" where
+"dest_del_ts dts== (
+case dts of Del_tree_stack ts => ts)
+"
+(*end delete types*)
+
+(*begin step del tree stack*)
+definition update_del_focus_at_position :: "node_t => nat => del_focus_t => del_focus_t" where
+"update_del_focus_at_position n i f == (
+let (ks,rs) = n in
+case f of
+DUp t => 
+(let rs2 = dest_Some(list_replace_1_at_n rs i t) in
+DUp(Node(ks,rs2)))
+| DDelete (t,d_index) => 
+(* I need to delete in focus_t, and restructure by using rs if the children size of focus_t is too small*)
+let t' = (remove_key_child d_index t) in
+(*now I need to check if the current sizes require a steal or a merge*)
+
+f
+)
+"
+
+
+definition step_del_tree_stack :: "del_tree_stack => del_tree_stack option" where
+"step_del_tree_stack ts == (
+let (f,stk) = dest_del_ts ts in
+case stk of 
+Nil => None
+| ((lb,(n,i),rb)#xs) => (
+let f2 = update_del_focus_at_position n i f in
+Some(Del_tree_stack(f2,xs))
+)
+
+)
+
+"
+(*end step del tree stack*)
+(*END*)
+
+(*BEGIN delete wf statements*)
+definition wellformed_del_focus :: "del_focus_t => bool => bool" where
+"wellformed_del_focus f stack_empty == (
+case f of
+DUp t => (wellformed_tree (Rmbs stack_empty) t)
+| DDelete (t,_) => (wellformed_tree (Rmbs stack_empty) t)  
+)"
+
+definition wellformed_del_ts :: "del_tree_stack => bool" where
+"wellformed_del_ts ts == (
+let (f,stk) = dest_del_ts ts in
+wellformed_del_focus f (stk=[])
+& wellformed_context stk
+(* maybe later..
+& wellformed_del_ts_1 ts*))"
+(*END delete wf statements*)
+end
