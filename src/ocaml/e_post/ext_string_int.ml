b3291ad70425e4c1724ce05cef93c067aa5116fa
Tom Ridge <tom.j.ridge@googlemail.com>
2017-02-13 13:24:57 +0000

    +

--- src/ocaml/e_post/ext_string_int.ml
+++ src/ocaml/e_post/ext_string_int.ml
@@ -6,65 +6,52 @@ open Ext_block_device
 
 (* assumptions ---------------------------------------- *)
 
-let block_size = Block.size
-
-let int_size = 4 (* bytes *)
-
 let digest_size = 128/8 (* bytes *)
 
 let key_size = digest_size
 
 
-(* Btree.Simple.Make() ------------------------------------------- *)
+(* instantiate Btree.Simple.Make() ----------------------------------------- *)
 
-open Btree.Simple  
+module Make = functor (ST:Btree_api.Simple.STORE) -> struct
 
-module Make = functor (ST:STORE) -> struct
+  module Simple = Btree_simple.Make(
+    struct 
 
-  module S = struct 
+      module KV = struct
 
-    module KV = struct
+        (* we map string to digest using hash *)
+        type pre_key = string
 
-      (* we map string to int using hash *)
-      type pre_key = string
+        type digest_t = string  [@@deriving yojson]
 
-      type digest_t = string[@@deriving yojson]
+        type key = digest_t  [@@deriving yojson] (* 16 char strings *)
 
-      type key = digest_t[@@deriving yojson] (* 16 char strings *)
+        type value = int  [@@deriving yojson]
 
-      type value_t = int[@@deriving yojson]
+        let key_ord = Digest.compare
 
-      let key_ord = Digest.compare
+        let equal_value x y = (x:int) = y
 
-      let equal_value x y = (x:int) = y
+      end (* KV *)
 
-    end (* KV *)
-    
-    let _ = (module KV : Btree.KEY_VALUE_TYPES)
+      let _ = (module KV : Btree.KEY_VALUE_TYPES)
 
-    module ST=ST
+      module ST=ST
 
-    module Sz : Sz_t = struct
-      let b : int = block_size
-      let k : int = 128 / 32
-      let v : int = 1
-      let r : int = 1
-    end
+      open KV
+      let pp: (key,value) Btree_api.Pickle_params.t = Pickle.(
+        {
+          p_k = (fun k -> Examples.p_string k);
+          u_k = (Examples.u_string key_size);
+          k_len = key_size;
+          p_v = Examples.p_int;
+          u_v = Examples.u_int;
+          v_len = key_size
+        })
 
-    module X = struct
-      open Btree_util
-      open M_int32s
-      let i : int cnv = X.i
-      let k = X.s
-      let v = X.i
-      let r = X.i
-    end
-
-  end (* S *)
-
-  (* check type *)
-  let _ = (module S: Btree.Simple.S)
-
-  module Simple' = Btree.Simple.Make(S)
+    end) (* Make *)
 
 end
+
+(* FIXME check result type *)
