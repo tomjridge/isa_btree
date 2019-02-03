b3c297a23e48cd58a72e03f21231bb2c6a7b22ba
Tom Ridge <tom.j.ridge@googlemail.com>
2019-02-03 12:19:23 +0000

    +

--- src/f_insert_mutate/Insert_with_mutation.thy
+++ src/f_insert_mutate/Insert_with_mutation.thy
@@ -7,43 +7,10 @@ begin
 
 (* ops types -------------------------------------------- *)
 
-
-(* read, write, rewrite; rewrite may CoW to produce a new blk ptr *)
-datatype_record ('r,'blk,'t) blk_ops = 
-read :: "('r \<Rightarrow> ('blk,'t) MM)"
-  wrte:: "('blk \<Rightarrow> ('r,'t) MM)"
-  rewrite :: "('r \<Rightarrow> 'blk \<Rightarrow> ('r option,'t) MM)" 
-(*
-type_synonym ('r,'blk,'t) blk_ops = "(
-('r \<Rightarrow> ('blk,'t) MM) * 
-('blk \<Rightarrow> ('r,'t) MM) *
-('r \<Rightarrow> 'blk \<Rightarrow> ('r option,'t) MM))" 
-*)
-
-(* blk \<Rightarrow> dnode ; dnode \<Rightarrow> 'blk;  *)
-datatype_record ('blk,'k,'v,'r) marshal_ops = 
-blk2dnode :: "'blk \<Rightarrow> ('k,'v,'r) dnode"
-dnode2blk :: "('k,'v,'r) dnode \<Rightarrow> 'blk"
-
-(*
-type_synonym ('blk,'k,'v,'r) marshal_ops = "(
-('blk \<Rightarrow> ('k,'v,'r) dnode) * 
-(('k,'v,'r) dnode \<Rightarrow> 'blk))"
-*)
-
-(* alloc; free *) 
-datatype_record ('r,'t) alloc_ops = 
-alloc :: "unit \<Rightarrow> ('r,'t) MM"
-free :: "'r s \<Rightarrow> (unit,'t) MM" 
-
-(*
-type_synonym ('r,'t) alloc_ops = "(
-(unit \<Rightarrow> ('r,'t) MM) *
-('r s \<Rightarrow> (unit,'t) MM))"
-*)
-
-
-
+datatype_record ('r,'dnode,'t) store_ops =
+  read :: "'r \<Rightarrow> ('dnode,'t) MM"
+  wrte :: "'dnode \<Rightarrow> ('r,'t) MM"
+  rewrite :: "'r \<Rightarrow> 'dnode \<Rightarrow> ('r option,'t) MM"
 
 (* find ------------------------------- *)
 
@@ -51,18 +18,15 @@ type_synonym ('r,'t) alloc_ops = "(
 definition find_step :: "
 constants \<Rightarrow> 
 'k ord \<Rightarrow> 
-('r,'blk,'t) blk_ops \<Rightarrow>
-('blk,'k,'v,'r) marshal_ops \<Rightarrow> 
-('r,'t) alloc_ops \<Rightarrow>  
+('r,('k,'v,'r)dnode,'t) store_ops \<Rightarrow>  
 ('k,'v,'r) find_state \<Rightarrow> (('k,'v,'r) find_state,'t) MM" where
-"find_step cs k_cmp blk_ops marshal_ops alloc_ops = (
-  let read = blk_ops|>read in
-  let blk2dnode = marshal_ops|>blk2dnode in
+"find_step cs k_cmp store_ops = (
+  let read = store_ops|>read in
   (% fs. 
   case fs of 
   F_finished _ \<Rightarrow> return fs \<comment> \<open> stutter; or fail? \<close>
   | F_down(r0,k,r,stk) \<Rightarrow> (
-    read r |>fmap blk2dnode |>fmap (% f. 
+    read r |>fmap (% f. 
     case f of 
     Disk_node (ks,rs) \<Rightarrow> (
       let (frm,r) = make_frame k_cmp k r ks rs in      
@@ -78,12 +42,10 @@ constants \<Rightarrow>
 definition step_down :: "
 constants \<Rightarrow> 
 'k ord \<Rightarrow> 
-('r,'blk,'t) blk_ops \<Rightarrow>
-('blk,'k,'v,'r) marshal_ops \<Rightarrow> 
-('r,'t) alloc_ops \<Rightarrow> 
+('r,('k,'v,'r)dnode,'t) store_ops \<Rightarrow>
 ('k,'v,'r) d \<Rightarrow> (('k,'v,'r) d,'t) MM" where
-"step_down cs k_cmp blk_ops marshal_ops alloc_ops = (
-  let find_step =  find_step cs k_cmp blk_ops marshal_ops alloc_ops in
+"step_down cs k_cmp store_ops = (
+  let find_step =  find_step cs k_cmp store_ops in
   (% d.
   let (fs,v) = d in
   find_step fs |> fmap (% d'. (d',v)) ))"
@@ -92,14 +54,10 @@ constants \<Rightarrow>
 definition step_bottom :: "
 constants \<Rightarrow> 
 'k ord \<Rightarrow> 
-('r,'blk,'t) blk_ops \<Rightarrow>
-('blk,'k,'v,'r) marshal_ops \<Rightarrow> 
-('r,'t) alloc_ops \<Rightarrow> 
+('r,('k,'v,'r)dnode,'t) store_ops \<Rightarrow>
 ('k,'v,'r) d \<Rightarrow> (('k,'v,'r) u + unit,'t) MM" where
-"step_bottom cs k_cmp blk_ops marshal_ops alloc_ops d = (
-  let (write,rewrite) = (blk_ops|>wrte,blk_ops|>rewrite) in
-  let dnode2blk = marshal_ops|>dnode2blk in
-  let alloc = alloc_ops|>alloc in
+"step_bottom cs k_cmp store_ops d = (
+  let (write,rewrite) = (store_ops|>wrte,store_ops|>rewrite) in
   let (fs,v) = d in
   case dest_F_finished fs of 
   None \<Rightarrow> impossible1 (STR ''insert, step_bottom'')
@@ -109,34 +67,27 @@ constants \<Rightarrow>
     case length kvs' \<le> (cs|>max_leaf_size) of
     True \<Rightarrow> (
       \<comment> \<open> we want to update in place if possible \<close>
-      Disk_leaf kvs' |> dnode2blk |> (% blk. 
-      rewrite r blk |> bind (% r'. 
+      Disk_leaf kvs' |> rewrite r |> bind (% r'. 
       case r' of 
       None \<Rightarrow> 
         \<comment> \<open> block was updated in place \<close>
         return (Inr ())
-      | Some r' \<Rightarrow> return (Inl(I1 r',stk)))))
+      | Some r' \<Rightarrow> return (Inl(I1 r',stk))))
     | False \<Rightarrow> (
       let (kvs1,k',kvs2) = split_leaf cs kvs' in
-      Disk_leaf kvs1 |> dnode2blk |> (% blk.
-      write blk |> bind (% r1.
-      Disk_leaf kvs2 |> dnode2blk |> (% blk. 
-      write blk |> bind (% r2. 
-      return (Inl(I2(r1,k',r2),stk)))))))))"
+      Disk_leaf kvs1 |> write |> bind (% r1.
+      Disk_leaf kvs2 |> write |> bind (% r2. 
+      return (Inl(I2(r1,k',r2),stk)))))))"
 
 
 
 definition step_up :: "
 constants \<Rightarrow> 
 'k ord \<Rightarrow> 
-('r,'blk,'t) blk_ops \<Rightarrow>
-('blk,'k,'v,'r) marshal_ops \<Rightarrow> 
-('r,'t) alloc_ops \<Rightarrow> 
+('r,('k,'v,'r)dnode,'t) store_ops \<Rightarrow>
 ('k,'v,'r) u \<Rightarrow> (('k,'v,'r) u + unit,'t) MM" where
-"step_up  cs k_cmp blk_ops marshal_ops alloc_ops u = (
-  let (write,rewrite) = (blk_ops|>wrte,blk_ops|>rewrite) in
-  let dnode2blk = marshal_ops|>dnode2blk in
-  let alloc = alloc_ops|>alloc in
+"step_up  cs k_cmp store_ops u = (
+  let (write,rewrite) = (store_ops|>wrte,store_ops|>rewrite) in
   let (fo,stk) = u in
   case stk of 
   [] \<Rightarrow> impossible1 (STR ''insert, step_up'') 
@@ -145,43 +96,35 @@ constants \<Rightarrow>
     case fo of
     I1 r \<Rightarrow> (
       let (ks,rs) = unsplit_node ((rs1,ks1),([r],[]),(ks2,rs2)) in
-      Disk_node(ks,rs) |> dnode2blk |> (% blk.
-      rewrite r_parent blk |> bind (% r2. 
+      Disk_node(ks,rs) |> rewrite r_parent |> bind (% r2. 
       case r2 of 
       None \<Rightarrow> return (Inr ())
-      | Some r2 \<Rightarrow> return (Inl (I1 r2, stk')))))
+      | Some r2 \<Rightarrow> return (Inl (I1 r2, stk'))))
     | I2 (r1,k,r2) \<Rightarrow> (
       let (ks',rs') = unsplit_node ((rs1,ks1), ([r1,r2],[k]), (ks2,rs2)) in
       case List.length ks' \<le> (cs|>max_node_keys) of
       True \<Rightarrow> (
-        Disk_node(ks',rs') |> dnode2blk |> (% blk.
-        rewrite r_parent blk |> bind (% r2. 
+        Disk_node(ks',rs') |> rewrite r_parent |> bind (% r2. 
         case r2 of 
         None \<Rightarrow> return (Inr ())
-        | Some r2 \<Rightarrow> return (Inl (I1 r2, stk')))))
+        | Some r2 \<Rightarrow> return (Inl (I1 r2, stk'))))
       | False \<Rightarrow> (
         let (ks_rs1,k,ks_rs2) = split_node cs (ks',rs') in  
-        Disk_node(ks_rs1) |> dnode2blk |> (% blk.
-        write blk |> bind (% r1. 
-        Disk_node (ks_rs2) |> dnode2blk |> (% blk.
-        write blk |> bind (% r2.
-        return (Inl (I2(r1,k,r2),stk')))) ))))))"
+        Disk_node(ks_rs1) |> write |> bind (% r1. 
+        Disk_node (ks_rs2) |> write |> bind (% r2.
+        return (Inl (I2(r1,k,r2),stk')))) ))))"
 
 
 definition insert_step :: "
 constants \<Rightarrow> 
 'k ord \<Rightarrow> 
-('r,'blk,'t) blk_ops \<Rightarrow>
-('blk,'k,'v,'r) marshal_ops \<Rightarrow> 
-('r,'t) alloc_ops \<Rightarrow> 
+('r,('k,'v,'r)dnode,'t) store_ops \<Rightarrow>
 ('k,'v,'r) insert_state \<Rightarrow> (('k,'v,'r) insert_state,'t) MM" where
-"insert_step cs k_cmp  blk_ops marshal_ops alloc_ops = (
-  let step_down = step_down cs k_cmp  blk_ops marshal_ops alloc_ops in
-  let step_bottom = step_bottom cs k_cmp  blk_ops marshal_ops alloc_ops in
-  let step_up = step_up  cs k_cmp blk_ops marshal_ops alloc_ops in
-  let write = blk_ops|>wrte in
-  let dnode2blk = marshal_ops|>dnode2blk in
-  let alloc = alloc_ops|>alloc in
+"insert_step cs k_cmp store_ops = (
+  let step_down = step_down cs k_cmp store_ops in
+  let step_bottom = step_bottom cs k_cmp store_ops in
+  let step_up = step_up  cs k_cmp store_ops in
+  let write = store_ops|>wrte in
   (% s.
   case s of 
   I_down d \<Rightarrow> (
@@ -199,9 +142,8 @@ constants \<Rightarrow>
       case fo of 
       I1 r \<Rightarrow> return (I_finished r)
       | I2(r1,k,r2) \<Rightarrow> (
-        (Disk_node([k],[r1,r2]) |> dnode2blk |> (% blk.
-        write blk |> bind (% r.
-        return (I_finished r))))))
+        (Disk_node([k],[r1,r2]) |> write |> bind (% r.
+        return (I_finished r)))))
     | _ \<Rightarrow> (step_up u |> bind (% u. 
       case u of 
       Inr () \<Rightarrow> return I_finished_with_mutate
@@ -215,9 +157,6 @@ export_code
 fmap
 Disk_node
 make_constants
-make_alloc_ops
-make_blk_ops
-make_marshal_ops
 make_initial_find_state
 I1
 I_down
