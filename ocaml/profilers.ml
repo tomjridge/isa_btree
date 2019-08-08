20192c91da72fd1ff1b0eca3d46907b6781ce987
Tom Ridge <tom.j.ridge@googlemail.com>
2019-08-08 21:21:03 +0100

    +

--- ocaml/profilers.ml
+++ ocaml/profilers.ml
@@ -1,13 +1,13 @@
 (** Various profilers. *)
 
 module Leaf_profiler = struct
-  let { mark; time_thunk; _ } = make_profiler ()
+  let { mark; time_thunk; _ } = make_profiler ~print_header:"leaf profiler" ()
 end
 
 module Node_profiler = struct
-  let { mark; time_thunk; _ } = make_profiler ()
+  let { mark; time_thunk; _ } = make_profiler ~print_header:"node profiler" ()
 end
 
 module Frame_profiler = struct
-  let { mark; time_thunk; _ } = make_profiler ()
+  let { mark; time_thunk; _ } = make_profiler ~print_header:"frame profiler" ()
 end
