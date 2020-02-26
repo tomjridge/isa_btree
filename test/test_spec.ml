089bf066f779693da01415850e22acef75640e8a
Tom Ridge <tom.j.ridge@googlemail.com>
2020-02-26 15:58:46 +0000

    +

--- test/test_spec.ml
+++ test/test_spec.ml
@@ -2,4 +2,4 @@
 
 let k_cmp : int -> int -> int = Stdlib.compare
 
-let spec_map_ops : (int,int,(int,int,unit)Tjr_map.map) Tjr_map.map_ops = Tjr_map.make_map_ops k_cmp
+let spec_map_ops : (int,int,(int,int,unit)Tjr_map.map) Tjr_map.map_ops = Tjr_map.unsafe_make_map_ops k_cmp
